1bcbcff7f1861eb71ce1f0417dd979c6fcc6eec5
Brian Bollig <bjb@procaresoftware.com>
2019-12-30 13:30:07 -0700

    Some notes

--- Loans.Tests/LoanTermShould.cs
+++ Loans.Tests/LoanTermShould.cs
@@ -9,6 +9,10 @@ namespace Loans.Tests
         [Test]
         public void ReturnTermInMonths()
         {
+            //Constraint method of assertion is the new feature
+            //Assert.That(*test result, *constraint instance);
+            //vs. Classic method of assertion which would look like this:
+            //Assert.AreEqual(1, sut.Years);
             var sut = new LoanTerm(1);
             Assert.That(sut.ToMonths(), Is.EqualTo(12));
         }
