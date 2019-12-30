f95bd533881afe0d534094d99cde7456bd482408
Brian Bollig <bjb@procaresoftware.com>
2019-12-30 14:10:04 -0700

    Test to see if each item in collection is not duplicated.

--- Loans.Tests/ProductComparerShould.cs
+++ Loans.Tests/ProductComparerShould.cs
@@ -29,5 +29,23 @@ namespace Loans.Tests
             Assert.That(comparisons, Has.Exactly(3).Items);
         }
 
+        [Test]
+        public void NotReturnDuplicateComparisons()
+        {
+            //Arrange
+            var products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3),
+            };
+
+            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            Assert.That(comparisons, Is.Unique);
+        }
     }
 }
