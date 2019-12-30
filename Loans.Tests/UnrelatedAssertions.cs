16d33ee7794f5fbc539bfa28cd955a3286f4ecd6
Brian Bollig <bjb@procaresoftware.com>
2019-12-30 15:25:51 -0700

    Boolean Assertions, Range Assertions and Date Range Assertions

--- Loans.Tests/UnrelatedAssertions.cs
+++ Loans.Tests/UnrelatedAssertions.cs
@@ -8,6 +8,9 @@ namespace Loans.Tests
     [TestFixture]
     class UnrelatedAssertions
     {
+
+        //for and exhaustive list, head here:
+        //https://github.com/nunit/docs/wiki/Constraints
         [Test]
         public void StringTestsExamples()
         {
@@ -38,5 +41,44 @@ namespace Loans.Tests
             Assert.That(name, Does.StartWith("xyz")
                 .Or.EndWith("rah"));
         }
+
+        [Test]
+        public void BooleanTestsExamples()
+        {
+            bool isNew = true;
+
+            Assert.That(isNew);
+            Assert.That(isNew, Is.True);
+
+            bool areMarried = false;
+            
+            Assert.That(areMarried == false);
+            Assert.That(areMarried, Is.False);
+            Assert.That(areMarried, Is.Not.True);
+        }
+
+        [Test]
+        public void RangeTestsExamples()
+        {
+            int i = 42;
+
+            //Assert.That(i, Is.GreaterThan(42));
+            Assert.That(i, Is.GreaterThanOrEqualTo(42));
+            //Assert.That(i, Is.LessThan(42));
+            Assert.That(i, Is.LessThanOrEqualTo(42));
+            Assert.That(i, Is.InRange(40, 50));
+        }
+
+        [Test]
+        public void DateRangeTestsExamples()
+        {
+            DateTime d1 = new DateTime(2000, 2, 20);
+            DateTime d2 = new DateTime(2000, 2, 25);
+
+            //Assert.That(d1, Is.EqualTo(d2));
+            //Assert.That(d1, Is.EqualTo(d2).Within(2).Days);
+            Assert.That(d1, Is.EqualTo(d2).Within(5).Days);
+        }
+
     }
 }
