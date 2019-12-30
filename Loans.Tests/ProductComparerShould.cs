44e49f5af7f4734330c4f3e4253db4f332eb3683
Brian Bollig <bjb@procaresoftware.com>
2019-12-30 14:29:24 -0700

    Can chain extension methods to check individual property information

--- Loans.Tests/ProductComparerShould.cs
+++ Loans.Tests/ProductComparerShould.cs
@@ -69,5 +69,31 @@ namespace Loans.Tests
 
             Assert.That(comparisons, Does.Contain(expectedProduct));
         }
+
+        [Test]
+        public void ReturnComparisonForFirstProduct_WithPartialKnownExpectedValues()
+        {
+            //Arrange
+            var products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3),
+            };
+
+            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            //Don't care about the monthly repayment, only that the product is there
+            Assert.That(comparisons, Has.Exactly(1)
+                                                    .Property("ProductName").EqualTo("a")
+                                                    .And
+                                                    .Property("InterestRate").EqualTo(1)
+                                                    .And
+                                                    .Property("MonthlyRepayment").GreaterThan(0));
+        }
+
     }
 }
