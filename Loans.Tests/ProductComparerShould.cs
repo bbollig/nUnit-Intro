4f93c265680fe56868918d0779e18f32956ffcd8
Brian Bollig <bjb@procaresoftware.com>
2019-12-30 14:09:08 -0700

    Adding ProductComparer test class with method for testing collections

---
+++ Loans.Tests/ProductComparerShould.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Loans.Domain.Applications;
+using NUnit.Framework;
+
+namespace Loans.Tests
+{
+    [TestFixture]
+    public class ProductComparerShould
+    {
+        [Test]
+        public void ReturnCorrectNumberOfComparisons()
+        {
+            //Arrange
+            var products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3),
+            };
+
+            var sut =  new ProductComparer(new LoanAmount("USD", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            //Has constraint helper class has a number of convenience methods for working with collections
+            Assert.That(comparisons, Has.Exactly(3).Items);
+        }
+
+    }
+}
