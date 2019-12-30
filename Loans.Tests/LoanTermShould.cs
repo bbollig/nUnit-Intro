7b76033170d0301c5831ef59f5041867518c1556
Brian Bollig <bjb@procaresoftware.com>
2019-12-30 13:43:28 -0700

    Asserting on Reference Equality

--- Loans.Tests/LoanTermShould.cs
+++ Loans.Tests/LoanTermShould.cs
@@ -1,5 +1,6 @@
 ﻿using Loans.Domain.Applications;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Loans.Tests
 {
@@ -43,6 +44,25 @@ namespace Loans.Tests
 
             Assert.That(a, Is.Not.EqualTo(b));
         }
+
+        [Test]
+        public void ReferenceEqualityExample()
+        {
+            var a = new LoanTerm(1);
+            var b = a;
+            var c =  new LoanTerm(1);
+
+            Assert.That(a, Is.SameAs(b));
+            Assert.That(a, Is.Not.SameAs(c));
+
+            var x = new List<string> {"a", "b"};
+            var y = x;
+            var z = new List<string> { "a", "b" };
+
+            Assert.That(y, Is.SameAs(x));
+            Assert.That(z, Is.Not.SameAs(x));
+        }
+
 
     }
 }
