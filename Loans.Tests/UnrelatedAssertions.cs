5bc8fda841c076b15d715b3751faf6af5c3e8453
Brian Bollig <bjb@procaresoftware.com>
2019-12-30 15:11:32 -0700

    Examples of assertions using strings

---
+++ Loans.Tests/UnrelatedAssertions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Loans.Tests
+{
+    [TestFixture]
+    class UnrelatedAssertions
+    {
+        [Test]
+        public void StringTestsExamples()
+        {
+            //Commenting out failing Assertions but leaving for examples
+            string name = "Sarah";
+
+            //Assert.That(name, Is.Empty);//fail
+            
+            Assert.That(name, Is.Not.Empty);
+            
+            Assert.That(name, Is.EqualTo("Sarah"));
+            
+            //Assert.That(name, Is.EqualTo("SARAH"));//fail
+            
+            Assert.That(name, Is.EqualTo("SARAH").IgnoreCase);
+            
+            Assert.That(name, Does.StartWith("Sa"));
+            
+            Assert.That(name, Does.EndWith("ah"));
+            
+            Assert.That(name, Does.Contain("ara"));
+            
+            Assert.That(name, Does.Not.Contain("Brian"));
+            
+            Assert.That(name, Does.StartWith("Sa")
+                .And.EndWith("rah"));
+
+            Assert.That(name, Does.StartWith("xyz")
+                .Or.EndWith("rah"));
+        }
+    }
+}
