cdc190a558ceeb0e72fcff09c2896ea9f87cbae0
Brian Bollig <bjb@procaresoftware.com>
2019-12-30 13:37:08 -0700

    Asserting on non-reference type equality

--- Loans.Tests/LoanTermShould.cs
+++ Loans.Tests/LoanTermShould.cs
@@ -23,5 +23,26 @@ namespace Loans.Tests
             var sut = new LoanTerm(1);
             Assert.That(sut.Years, Is.EqualTo(1));
         }
+
+        [Test]
+        public void RespectValueEquality()
+        {
+            var a = new LoanTerm(1);
+            var b = new LoanTerm(1);
+
+            Assert.That(a, Is.EqualTo(b));
+
+
+        }
+
+        [Test]
+        public void RespectValueInEquality()
+        {
+            var a = new LoanTerm(1);
+            var b = new LoanTerm(2);
+
+            Assert.That(a, Is.Not.EqualTo(b));
+        }
+
     }
 }
