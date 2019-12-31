14e62c7821449c0e91f1b937972ee1b1a8ec1a41
Brian Bollig <bjb@procaresoftware.com>
2019-12-31 09:06:15 -0700

    Example implemetation of OneTimeSetUp and OneTimeTearDown methods

--- Loans.Tests/ProductComparerShould.cs
+++ Loans.Tests/ProductComparerShould.cs
@@ -9,18 +9,35 @@ namespace Loans.Tests
         private List<LoanProduct> products;
         private ProductComparer sut;
 
-        //Runs BEFORE EACH test executes
-        [SetUp]
-        public void SetUp()
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
         {
-            //Arrange
+            //In cases where creating a resource takes a large amount of time and we do not want to
+            //repeat it for every test, we can use OneTimeSetUp
+            //Simulate long setup init time for this list of products
+            //We assume that this list will not be modified by any tests
+            //as this will potentially break other tests (i.e. break test isolation)
             products = new List<LoanProduct>
             {
                 new LoanProduct(1, "a", 1),
                 new LoanProduct(2, "b", 2),
                 new LoanProduct(3, "c", 3),
             };
+        }
 
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            //Run after lsat test in this test class (fixture) executes
+            //e.g. disposing of shared expensive setup performed in OneTimeSetUp
+
+            //products.Dispose(); e.g. if products implemented IDisposable
+        }
+
+        //Runs BEFORE EACH test executes
+        [SetUp]
+        public void SetUp()
+        {
             sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
         }
 
