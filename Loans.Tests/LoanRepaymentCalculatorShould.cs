911d195ad398feeaaa074ca2ff667d8a7bc2bb68
Brian Bollig <bjb@procaresoftware.com>
2019-12-31 11:06:16 -0700

    Examples of creating test data using NUnit's own framework

--- Loans.Tests/LoanRepaymentCalculatorShould.cs
+++ Loans.Tests/LoanRepaymentCalculatorShould.cs
@@ -77,5 +77,53 @@ namespace Loans.Tests
 
             Assert.That(monthlyPayment, Is.EqualTo(expectedMonthlyPayment));
         }
+
+        [Test]
+        public void CalculateCorrectMonthlyRepayment_Combinatorial(
+            [Values(100_000, 200_000, 500_000)] decimal principal,
+            [Values(6.5, 10, 20)] decimal interestRate,
+            [Values(10, 20, 30)] int termInYears)
+        {
+            //When we use NUnit's [Values] attributes, the framework will create combinations of these attributes automatically
+            //This will generate 27 test cases for us.
+            var sut = new LoanRepaymentCalculator();
+            var monthlyPayment = sut.CalculateMonthlyRepayment(
+                new LoanAmount("USD", principal), interestRate, new LoanTerm(termInYears));
+
+            //However, this precludes adding an Assertion. What this allows us to test is a wide array of values to check the sut
+            //doesn't throw an unanticipated exception 
+        }
+
+        [Test]
+        [Sequential]
+        public void CalculateCorrectMonthlyRepayment_Sequential(
+            [Values(200_000, 200_000, 500_000)] decimal principal,
+            [Values(6.5, 10, 10)] decimal interestRate,
+            [Values(30, 30, 30)] int termInYears,
+            [Values(1264.14, 1755.14, 4387.86)] decimal expectedMonthlyPayment)
+        {
+            //Using the [Sequential] decoration enforces a sequential order of use of [Values], allowing
+            //us to add our Assertion statement back in
+            var sut = new LoanRepaymentCalculator();
+            var monthlyPayment = sut.CalculateMonthlyRepayment(
+                new LoanAmount("USD", principal), interestRate, new LoanTerm(termInYears));
+
+            Assert.That(monthlyPayment, Is.EqualTo(expectedMonthlyPayment));
+        }
+
+        [Test]
+        public void CalculateCorrectMonthlyRepayment_Range(
+            [Range(50_000, 1_000_000, 50_000)] decimal principal,
+            [Range(0.5, 20.00, 0.5)] decimal interestRate,
+            [Values(10, 20, 30)] int termInYears)
+        {
+            //Here we can specify Ranges of values to be used that will also generate test cases 
+            //automatically for us using the combinatorial approach. With the Range values we've 
+            //specified here, 2400 tests are generated.
+            var sut = new LoanRepaymentCalculator();
+            var monthlyPayment = sut.CalculateMonthlyRepayment(
+                new LoanAmount("USD", principal), interestRate, new LoanTerm(termInYears));
+        }
+
     }
 }
